aea5fd842302c7daabc109949b3c38aba54877dd
Reroreo1 <86200459+reroreo1@users.noreply.github.com>
2022-12-07 22:31:34 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,34 +6,28 @@
 /*   By: rezzahra <rezzahra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 23:20:53 by rezzahra          #+#    #+#             */
-/*   Updated: 2022/12/06 12:06:45 by rezzahra         ###   ########.fr       */
+/*   Updated: 2022/12/07 22:28:51 by rezzahra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Response.hpp"
-static void tokenize(std::string str, std::string delim, std::pair<std::string,std::string> &rhs){
-	if (str.find(delim) != string::npos){
-		rhs.first = str.substr(0,str.find(delim));
-		rhs.second = str.substr(str.find(delim) + delim.length(),str.length() - rhs.first.length() - delim.length());
-	}
+std::string unts(std::uint16_t n){
+	std::stringstream ss;
+	ss << n;
+	return (ss.str());
 }
 
-std::map<std::string,std::string> fillMap(void){
-	std::map<std::string,std::string> mime;
-	std::string delim,value,key,line,content = "./mime.txt";
-	delim = "===";
-	std::pair<std::string,std::string> rhs;
-	ifstream file1;
-	file1.open(content);
-	while (!file1.eof() && getline(file1,line, '\n')){
-		tokenize(line,delim,rhs);
-		mime.insert(rhs);
-	}
-	return (mime);
-}
+std::string& makeHeader(Response &rhs,Request &lhs){
+	std::string header;
+	std::map<std::string,std::string>::iterator it = lhs.hd.hd.find("Connection");
+	header = rhs.Code.HTTPv + " " + unts(rhs.Code.code) + " " + rhs.Code.reason + "\n";
+	header += it->first + ": " + it->second;
+	if (getMethod(lhs) == "GET"){
+		header += "Content: " + rhs.contentLength;
+} 
 
 int main(){
-	std::string r = "index.html";
-	std::map<std::string,std::string> mime = fillMap();
-	std::cout << mime.at(r.substr(r.find("."),std::string::npos));
+	std::string p = "rachid";
+	std::string a = "daddy";
+	std::cout << p + " " + a << std::endl;
 }
