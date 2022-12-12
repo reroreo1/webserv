ec25c7e78cb0be2970f5931efe93a7657d846fd0
Reroreo1 <86200459+skyjumper24@users.noreply.github.com>
2022-12-12 19:05:40 +0100

    delete tested

--- Delete.cpp
+++ Delete.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Delete.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rezzahra <rezzahra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mac <mac@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 23:20:53 by rezzahra          #+#    #+#             */
-/*   Updated: 2022/12/11 16:54:43 by rezzahra         ###   ########.fr       */
+/*   Updated: 2022/12/12 19:02:39 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,33 @@
 #include <ftw.h>
 #include <unistd.h>
 
+int unlink_cb1(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    struct stat st;
+    stat(fpath, &st);
+    if (!(st.st_mode & S_IWUSR))
+        return -1;
+    return 0;
+}
+
 int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
     int rv = remove(fpath);
-
     if (rv)
         perror(fpath);
-
     return rv;
 }
 
 int rmrf(char *path)
 {
-    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+    return nftw(path, unlink_cb1, 64, FTW_DEPTH | FTW_PHYS);
+}
+int rmrf1(char *path)
+{
+    return nftw(path, unlink_cb1, 64, FTW_DEPTH | FTW_PHYS);
 }
 
-int main(){
-	rmrf("./pop");
-}
 
 int main(){
-	
+	std::cout << rmrf("./helperfile") << std::endl;
 }
